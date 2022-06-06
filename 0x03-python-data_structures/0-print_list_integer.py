f98ea33a21e7782931d37ff29732e88c0cb8f374
nejatahm <nejah2005@gmail.com>
2022-06-05 17:36:52 -0700

    print list of all integers

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin
+def print_list_integer(my_list=[]):
+    my_list = list([start], [end], [step])
+    for item in my_list:
+        print(item)
+
+
+if __name__ == '__main__':
+    print(str.format(my_list))
+    print_list_integer(my_list=[])
