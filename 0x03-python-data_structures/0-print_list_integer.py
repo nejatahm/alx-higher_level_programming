eb40efb9545b786276e5f1dd452324805a0292d3
nejatahm <nejah2005@gmail.com>
2022-06-05 17:45:29 -0700

    all integer in list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin
 def print_list_integer(my_list=[]):
-    my_list = list([start], [end], [step])
     for item in my_list:
-        print(item)
+        print("{:d}".format(item))
 
 
-if __name__ == '__main__':
-    print(str.format(my_list))
-    print_list_integer(my_list=[])
+
