afa96c925e46cdc39ec63e426dadffd77d225c48
nejatahm <nejah2005@gmail.com>
2022-06-06 08:49:42 -0700

    list % 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,4 +3,5 @@ def divisible_by_2(my_list=[]):
     new_list = []
     if my_list:
         for elm in my_list:
-            new_list.append(False if elm % 2 else True)
+            new_list.append(True if elm % 2 else False)
+    return new_list
