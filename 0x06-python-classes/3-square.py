bcf2a76d9f8741ad384b7cf864ce0207418374e2
nejatahm <nejah2005@gmail.com>
2022-06-23 17:27:40 -0700

    sqaure calculation

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/in/python
+"""Defines a class square"""
+
+
+class Square:
+    """Represents a square
+
+    Attributes:
+        __size(int): size of side of the square
+    """
+    def __init__(self, size=0)
+        """intializes the square
+
+        Args:
+            size(int): size of side of the square
+
+            Returns:
+                None
+            """
+            if type(size) is not int:
+                raise TypeError("size must be an integer")
+            else:
+                if size < 0:
+                    raise ValueError("size must be >=0")
+                else:
+                    self.__size = size
+            
+            def area(self):
+                """calculates the square's area
+
+                Returns:
+                    The area of the square
+                """
+                return(self.__size) ** 2
