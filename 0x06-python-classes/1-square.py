26da38e7d07e237946d1e8a5ebe729808a3f42cb
nejatahm <nejah2005@gmail.com>
2022-06-23 17:05:27 -0700

    intialize square object

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/pytho3
+"""Square class definition"""
+
+
+class Square:
+    """Represents a square
+
+    Attributes:
+        __size(int): size of side of the square
+    """
+    def __init__(self, size)
+        """intialize a square
+
+        Args:
+            size(int): size of side of the  square
+
+        Returns: None
+        """
+        self.__size = size
